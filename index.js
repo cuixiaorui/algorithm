c61e38d87cbb62ae4c9c4eb100520eb6431fdc5d
cuixiaorui <cuixiaorui@cuixiaoruideMacBook-Pro.local>
2018-02-11 11:00:35 +0800

    1.优化exchange函数；2.增加冒泡算法的调用

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let selectSort = require('./src/sort/selectSort.js');
+let bubbleSort = require('./src/sort/bubbleSort.js');
 let fsControl = require('./src/utils/fsControl.js');
 (async function main(){
     let list = await fsControl.readTxt('./res/input.txt');
@@ -9,4 +10,5 @@ function exec(list){
     console.time('selectSort');
     selectSort(list);
     console.timeEnd('selectSort');
+    console.log(list);
 }
