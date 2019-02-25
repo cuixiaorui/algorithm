a915cb826500dc564313345afbb1ce5fc762ab8c
崔效瑞 <cui_xiaorui@126.com>
2019-02-25 16:35:12 +0800

    add: leetcode -> minSubArrayLen

---
+++ src/array/minSubArrayLen.js
@@ -0,0 +1,46 @@
+/**
+ * 长度最小的子数组
+ * 给定一个含有 n 个正整数的数组和一个正整数 s ，找出该数组中满足其和 ≥ s 的长度最小的连续子数组。如果不存在符合条件的连续子数组，返回 0。
+ * 
+ * 示例: 
+ * 
+ * 输入: s = 7, nums = [2,3,1,2,4,3]
+ * 输出: 2
+ * 解释: 子数组 [4,3] 是该条件下的长度最小的连续子数组。
+ * 
+ * 进阶:
+ * 
+ * 如果你已经完成了O(n) 时间复杂度的解法, 请尝试 O(n log n) 时间复杂度的解法。
+ * @param {number} s
+ * @param {number[]} nums
+ * @return {number} 
+ */
+var minSubArrayLen = function (s, nums) {
+    //当前这个算法比较暴力，需要优化
+    //优化点：利用之前记录的结果，不要在重新计算（todo）
+    if (nums.length === 0) return 0;
+    let minValue = 0;
+
+    //i 是滑动窗口的左侧
+    let i = 0;
+    //i 是滑动串钩的右侧
+    let j = 0;
+    let tempSum = 0;
+    while (i < nums.length) {
+        if (tempSum < s && j < nums.length) {
+            tempSum += nums[j];
+            j++;
+        } else {
+            if (tempSum >= s) {
+                let k = j - i;
+                if (k < minValue || !minValue) minValue = k;
+            }
+            i++;
+            j = i;
+            tempSum = 0;
+        }
+    }
+    return minValue;
+};
+console.log(minSubArrayLen(7,[2,3,1,2,4,3]))
+// console.log(minSubArrayLen(11, [1, 2, 3, 4, 5]))
