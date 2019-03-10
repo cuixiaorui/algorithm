83ee368a35bb8a525dd1189618b134f9fb28b136
cuixiaorui <cui_xiaorui@126.com>
2019-03-10 13:45:37 +0800

    feat: searchMatrix 增加新解法

--- src/leetcode/searchMatrix.js
+++ src/leetcode/searchMatrix.js
@@ -15,6 +15,7 @@
  *   [10, 13, 14, 17, 24],
  *   [18, 21, 23, 26, 30]
  * ]
+ *[1,4,7,11,15,2,5,8,12,19,3,6,9,16,22,10,13,14,17,24,18,21,23,26,30] 
  * 给定 target = 5，返回 true。
  * 
  * 给定 target = 20，返回 false。
@@ -26,7 +27,7 @@ var searchMatrix = function(matrix, target) {
     //先确定小得范围
     //如果target得值   大于 matrix[i][0]   小于 matrix[i][matrix[i].length] 说明其值有可能在这个范围内，
     //因为当前范围内得值都是有序得，可使用二分查找搜索
-   
+    //优化点：可以把for循环改为二分查找确定行
     for(let i=0,len = matrix.length; i<len;i++){
         let list = matrix[i];
         let listLen = list.length -1;
@@ -62,3 +63,34 @@ var searchMatrix = function(matrix, target) {
     [18, 21, 23, 26, 30]
   ]
 console.log(searchMatrix(m,20));
+
+
+
+var searchMatrix1 = function(matrix, target) {
+    //更快得解法
+    // 1. 所锁定行
+    let len = matrix.length;
+    let i = 0;
+    let j = matrix[0].length - 1;
+    
+    
+    while(i < len && j >= 0){
+        if(matrix[i][j] === target)return true;
+        else{
+            if(matrix[i][j] > target){
+                j--;
+            }else{
+                i++;
+            }
+        }
+    }
+    return false;
+}
+
+// console.log(searchMatrix1(m,20));
+let mm = [[-5]]
+searchMatrix1(mm,-5)
+
+
+
+//TODO 解法三：  把二维数组转化成一维数组 然后进行二分查找
